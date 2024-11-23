dd9a9ab41e125b1661a23e607f997c38d7b5fc2f
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-23 08:40:29 +0530

    Test Ng Program

---
+++ src/test/java/TestNG/FirstDemo.java
@@ -0,0 +1,15 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class FirstDemo {
+	
+	@Test
+	public void get()
+	{
+		
+		System.out.println("This is My first Test Case");
+		
+	}
+
+}
